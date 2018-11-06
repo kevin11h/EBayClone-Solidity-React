d14bc805c6e1de013936e5cc6d0df22ad63be5b5
Kevin Hung <kevhung11@gmail.com>
2018-11-05 16:35:22 -0800

    Add files via upload

--- App.beautifulnavbar.js
+++ App.beautifulnavbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-import { Modal, Navbar, Button } from 'react-bootstrap';
+import { Navbar} from 'react-bootstrap';
 
 class App extends Component {
 
@@ -11,8 +11,8 @@ class App extends Component {
     super(props, context);
     this.web3 = new Web3(window.web3.currentProvider);
 
-   const address = '0x2aF524D5Dc77b1e70C4C85612E0bc5f1fb10F6C8'; // copied from console log of 'node deploy.js' 0x...
-    const abi = '[{"constant":true,"inputs":[],"name":"getNumberOfProducts","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"products","outputs":[{"name":"id","type":"uint256"},{"name":"seller","type":"address"},{"name":"buyer","type":"address"},{"name":"name","type":"string"},{"name":"description","type":"string"},{"name":"price","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_name","type":"string"},{"name":"_description","type":"string"},{"name":"_price","type":"uint256"}],"name":"sellProduct","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_id","type":"uint256"}],"name":"buyProduct","outputs":[],"payable":true,"stateMutability":"payable","type":"function"}]';// copy from console log of 'node deploy.js'  [{"constant":..., "inputs":[], "name":...}];
+    const address = '0xF4c1801B5EF9D02E3CB0B91B728eF24Bfb7b77C7'; // copied from console log of 'node deploy.js' 0x...
+    const abi = [{"constant":true,"inputs":[],"name":"getNumberOfProducts","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"products","outputs":[{"name":"id","type":"uint256"},{"name":"seller","type":"address"},{"name":"buyer","type":"address"},{"name":"name","type":"string"},{"name":"description","type":"string"},{"name":"price","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_name","type":"string"},{"name":"_description","type":"string"},{"name":"_price","type":"uint256"}],"name":"sellProduct","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_id","type":"uint256"}],"name":"buyProduct","outputs":[],"payable":true,"stateMutability":"payable","type":"function"}];// copy from console log of 'node deploy.js'  [{"constant":..., "inputs":[], "name":...}];
     this.eBayClone = new this.web3.eth.Contract(abi, address);
   }
 
@@ -26,7 +26,7 @@ class App extends Component {
   }
 
   async refreshContractDetails() {
-     const account = await this.web3.eth.getAccounts();
+     const accounts = await this.web3.eth.getAccounts();
      const user = accounts[0];
      const balance = this.web3.utils.fromWei(await this.web3.eth.getBalance(user), 'ether');
 
@@ -55,14 +55,9 @@ class App extends Component {
           </Navbar.Collapse>
         </Navbar>;
       </div>
-    )
-  }
-            
-        User: {this.state.user}
-        <br />
-        Balance: {this.state.balance}
-      </div>
     );
   }
 }
     
+
+export default App;
--- App.localcontractinstances.js
+++ App.localcontractinstances.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
+import {Navbar} from 'react-bootstrap';
 
 class App extends Component {
 
@@ -10,7 +11,7 @@ class App extends Component {
     super(props, context);
     this.web3 = new Web3(window.web3.currentProvider);
 
-    const address = '0xF4c1801B5EF9D02E3CB0B91B728eF24Bfb7b77C7'; // copied from console log of 'node deploy.js' 0x...
+   const address = '0x2aF524D5Dc77b1e70C4C85612E0bc5f1fb10F6C8'; // copied from console log of 'node deploy.js' 0x...
     const abi = [{"constant":true,"inputs":[],"name":"getNumberOfProducts","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"products","outputs":[{"name":"id","type":"uint256"},{"name":"seller","type":"address"},{"name":"buyer","type":"address"},{"name":"name","type":"string"},{"name":"description","type":"string"},{"name":"price","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_name","type":"string"},{"name":"_description","type":"string"},{"name":"_price","type":"uint256"}],"name":"sellProduct","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_id","type":"uint256"}],"name":"buyProduct","outputs":[],"payable":true,"stateMutability":"payable","type":"function"}];// copy from console log of 'node deploy.js'  [{"constant":..., "inputs":[], "name":...}];
     this.eBayClone = new this.web3.eth.Contract(abi, address);
   }
@@ -25,7 +26,7 @@ class App extends Component {
   }
 
   async refreshContractDetails() {
-     const accounts = await this.web3.eth.getAccounts();
+     const account = await this.web3.eth.getAccounts();
      const user = accounts[0];
      const balance = this.web3.utils.fromWei(await this.web3.eth.getBalance(user), 'ether');
 
@@ -37,10 +38,22 @@ class App extends Component {
 
   render() {
     return (
-     <div>
-        User: {this.state.user}
-        <br />
-        Balance: {this.state.balance}
+      <div className="App">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+
+              <a href="#home">eBay Clone</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Navbar.Text>
+              Signed in as: <Navbar.Link href="#">{this.state.user}</Navbar.Link>
+            </Navbar.Text>
+            <Navbar.Text pullRight>Balance: {this.state.balance}</Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>;
       </div>
     );
   }
